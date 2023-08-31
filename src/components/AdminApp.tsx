cce86385f8cbaf4d215f3f914be4cfd896cf366f
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2023-08-31 17:15:21 +0800

    add react admin

---
+++ src/components/AdminApp.tsx
@@ -0,0 +1,26 @@
+// in src/components/AdminApp.jsx
+"use client"; // only needed if you choose App Router
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const AdminApp = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource
+            name="users"
+            list={ListGuesser}
+            edit={EditGuesser}
+            recordRepresentation="name"
+        />
+        <Resource
+            name="posts"
+            list={ListGuesser}
+            edit={EditGuesser}
+            recordRepresentation="title"
+        />
+        <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+    </Admin>
+);
+
+export default AdminApp;
